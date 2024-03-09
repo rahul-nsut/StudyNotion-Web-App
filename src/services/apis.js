a534f52a43aa1fd3a6d949f026a1ccd731abd5cd
rahul-nsut <95232888+rahul-nsut@users.noreply.github.com>
2024-03-09 23:28:11 +0530

    Update apis.js
    changed

--- src/services/apis.js
+++ src/services/apis.js
@@ -2,11 +2,11 @@ const BASE_URL = process.env.REACT_APP_BASE_URL
 
 // AUTH ENDPOINTS
 export const endpoints = {
-  SENDOTP_API: BASE_URL + "/auth/sendotp",
-  SIGNUP_API: BASE_URL + "/auth/signup",
-  LOGIN_API: BASE_URL + "/auth/login",
-  RESETPASSTOKEN_API: BASE_URL + "/auth/reset-password-token",
-  RESETPASSWORD_API: BASE_URL + "/auth/reset-password",
+  SENDOTP_API: BASE_URL + "/Auth/sendotp",
+  SIGNUP_API: BASE_URL + "/Auth/signup",
+  LOGIN_API: BASE_URL + "/Auth/login",
+  RESETPASSTOKEN_API: BASE_URL + "/Auth/reset-password-token",
+  RESETPASSWORD_API: BASE_URL + "/Auth/reset-password",
 }
 
 // PROFILE ENDPOINTS
