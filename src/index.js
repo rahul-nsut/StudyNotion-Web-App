27780e5c17290abebc4df77abadadc34fa5d7049
rsoota-godaddy <rsoota@godaddy.com>
2025-03-26 21:35:05 +0530

    Automated changes

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 This is a new line added automatically.
+This is a new line added automatically.
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import rootReducer from "./reducer";
