85a9b3e9ba3088c0913729b9d72f832efa405ffb
rsoota-godaddy <rsoota@godaddy.com>
2025-03-26 21:48:46 +0530

    Automated changes

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+change this line.
 import App from "./App";
 This is a new line added automatically.
 This is a new line added automatically.
